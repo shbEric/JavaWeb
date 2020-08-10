d823e79ec76e11e4559b277ba77f65e624771036
shbEric <1289878918@qq.com>
2020-08-10 11:53:06 +0800

    请求转发RequestDispatcher

---
+++ WebDemo/src/com/super404/web/DispatcherServlet.java
@@ -0,0 +1,24 @@
+package com.super404.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/forward")
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("DispatcherServlet doGet");
+
+        //req.getRequestDispatcher("/WEB-INF/admin.jsp").forward(req, resp);
+
+        //存储java对象到request作用域，转发后一样可以获取到值，具体怎么获取？JSP或者EL表达式
+        req.setAttribute("name","jack");
+        req.getRequestDispatcher("requestServlet").forward(req, resp);
+    }
+}
---
+++ WebDemo/web/WEB-INF/admin.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Eric
+  Date: 2020/8/10
+  Time: 11:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+    admin.jsp
+
+</body>
+</html>
