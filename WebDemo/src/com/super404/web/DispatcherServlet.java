0e61d00110c7a933943afe9774ff02161002ff5d
shbEric <1289878918@qq.com>
2020-08-11 21:56:13 +0800

    请求重定向sendRedirect

--- WebDemo/src/com/super404/web/DispatcherServlet.java
+++ WebDemo/src/com/super404/web/DispatcherServlet.java
@@ -29,6 +29,15 @@ public class DispatcherServlet extends HttpServlet {
         user.setHost("https://xdclass.net");
         req.setAttribute("user", user);
 
-        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        //req.getRequestDispatcher("/index.jsp").forward(req, resp);
+
+
+        //请求重定向，不能访问WEB-INF下的文件，浏览器上的窗口地址会改版，可以用于跳转第三方地址或者应用里面的其他Servelt、jsp等
+        //重定向是取不到request中的存储的数据,如果当前servlet是重定向，浏览器可以看到两个请求
+
+        //重定向路径问题：如果没有加 http 开头，则认为是当前应用里面的servlet重定向，默认加上应用上下文；如果有加http则会使用配置的全路径进行跳转
+        //如果请求转发可以满足需要时，尽量使用请求转发，而不是重定向，效率性能更好
+        resp.sendRedirect("/index.jsp");
+        //resp.sendRedirect("https://xdclass.net");
     }
 }
