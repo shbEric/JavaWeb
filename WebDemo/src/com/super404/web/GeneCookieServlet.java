172ded99fcccd37294eadf078862357a21b2b217
shbEric <1289878918@qq.com>
2020-08-13 17:01:44 +0800

    Cookie

---
+++ WebDemo/src/com/super404/web/GeneCookieServlet.java
@@ -0,0 +1,24 @@
+package com.super404.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/gene_cookie_test")
+public class GeneCookieServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Cookie cookie = new Cookie("token", "fafwalcfdchkadkonac");
+        //20秒过期时间，过期后不会自动携带过去
+        cookie.setMaxAge(20);
+
+        response.addCookie(cookie);
+
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
---
+++ WebDemo/src/com/super404/web/GetCookieServlet.java
@@ -0,0 +1,25 @@
+package com.super404.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/get_cookie_test")
+public class GetCookieServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Cookie[] cookies = request.getCookies();
+
+        for (Cookie cookie : cookies) {
+            System.out.println(cookie.getDomain());
+            System.out.println(cookie.getName());
+            System.out.println(cookie.getValue());
+        }
+    }
+}
