a5fd4eb86c7f5407297e3ded5b26ee3e65bfc0f3
shbEric <1289878918@qq.com>
2020-10-21 19:31:59 +0800

    PrepareStatement预编译语句防sql注入

--- WebDemo/src/com/super404/web/dao/JDBCTest.java
+++ WebDemo/src/com/super404/web/dao/JDBCTest.java
@@ -2,6 +2,7 @@ package com.super404.web.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -33,9 +34,11 @@ public class JDBCTest {
         statement.close();
         connection.close();*/
 
-        testInjectSQL();
+        //testInjectSQL();
+        testPrepareStatement();
     }
 
+    //SQL注入攻击的例子
     private static void testInjectSQL() throws Exception{
         //加载JDBC驱动程序
         Class.forName("com.mysql.jdbc.Driver");
@@ -66,6 +69,39 @@ public class JDBCTest {
         //释放连接资源
         resultSet.close();
         statement.close();
+        connection.close();
+    }
+
+    //防止攻击的例子
+    private static void testPrepareStatement() throws Exception{
+        //加载JDBC驱动程序
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //建立数据库连接Connection
+        String username = "root";
+        String password = "123456";
+        //协议:子协议://ip:端口/数据库名称?参数1=值1&参数2=值2
+        String url = "jdbc:mysql://127.0.0.1:3306/xd_web?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        String name = "jack";
+        String pwd = "666' or 1=1 or'";
+        //创建执行SQL的语句Statement
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user where username=? and pwd=?");
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, pwd);
+
+
+        //处理执行结果ResultSet
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println("用户名称 name=" + resultSet.getString("username") +
+                    "  联系方式 wechat=" + resultSet.getString("wechat"));
+        }
+
+        //释放连接资源
+        resultSet.close();
+        preparedStatement.close();
         connection.close();
     }
 }
