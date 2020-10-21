b0216721d3c7c0ff2660ae6978cdc39ca918cc60
shbEric <1289878918@qq.com>
2020-10-21 19:20:41 +0800

    SQL注入攻击

--- WebDemo/src/com/super404/web/dao/JDBCTest.java
+++ WebDemo/src/com/super404/web/dao/JDBCTest.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class JDBCTest {
     public static void main(String[] args) throws Exception {
 
-        //加载JDBC驱动程序
+        /*//加载JDBC驱动程序
         Class.forName("com.mysql.jdbc.Driver");
 
         //建立数据库连接Connection
@@ -23,6 +23,41 @@ public class JDBCTest {
 
         //处理执行结果ResultSet
         ResultSet resultSet = statement.executeQuery("select * from user");
+        while (resultSet.next()) {
+            System.out.println("用户名称 name=" + resultSet.getString("username") +
+                    "  联系方式 wechat=" + resultSet.getString("wechat"));
+        }
+
+        //释放连接资源
+        resultSet.close();
+        statement.close();
+        connection.close();*/
+
+        testInjectSQL();
+    }
+
+    private static void testInjectSQL() throws Exception{
+        //加载JDBC驱动程序
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //建立数据库连接Connection
+        String username = "root";
+        String password = "123456";
+        //协议:子协议://ip:端口/数据库名称?参数1=值1&参数2=值2
+        String url = "jdbc:mysql://127.0.0.1:3306/xd_web?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        //创建执行SQL的语句Statement
+        Statement statement = connection.createStatement();
+
+        String name = "jack";
+        String pwd = "666' or 1=1 or'";
+
+        String sql = "select * from user where username='" + name + "' and pwd='" + pwd + "'";
+        System.out.println(sql);
+
+        //处理执行结果ResultSet
+        ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next()) {
             System.out.println("用户名称 name=" + resultSet.getString("username") +
                     "  联系方式 wechat=" + resultSet.getString("wechat"));
